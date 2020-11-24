82ff1c663717cec79868df11826494ba0ef21e38
Oohira Suguru <suguru.13579515@gmail.com>
2020-11-24 22:47:32 +0900

    Study python chapter3

---
+++ chapter3.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+a = 1 + 1 + 2
+print(a) 
+# コメントアウト
+
+A = 1 + 1
+print(A)
+
+print(a,A)
+
+b = 0.33
+B = 1.33
+sum = b + B
+print(sum)
+
+print(round(1.4))
+
+c = "こんにちわ"
+print(c)
+
+C = "私の\nの名前は"
+print(C)
+
+d = """
+aaa
+bbb
+ccc
+    """
+print(d)
+
+name = "鈴木" + "さん"
+print(name)
+
+price = str(2500) + "円"
+print(price)
+
+print("*" * 10)
+
+id = "abcdefg"
+print(id[2])
+print(id[:])
+print(id[2:])
+print(id[1:1+3])
+print(len(id))
+
+e = 3
+E = 3
+print(e == E)
+print(e != E)
+
+
+
