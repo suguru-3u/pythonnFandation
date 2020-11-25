2e81c35124be5aad05908375166e8d49de51a2ef
Oohira Suguru <suguru.13579515@gmail.com>
2020-11-25 21:45:13 +0900

    Study python

--- chapter4.py
+++ chapter4.py
@@ -1,39 +1,84 @@
-print(abs(-3.5))
+# print(abs(-3.5))
 
-print(min(4,3,6,1,))
+# print(min(4,3,6,1,))
 
-print(round(3.65))
+# print(round(3.65))
 
-a = "aaa"
-print(len(a))
+# a = "aaa"
+# print(len(a))
 
-# value = input("何か入力してください")
-# print(value)
+# # value = input("何か入力してください")
+# # print(value)
 
-import math
-print(math.ceil(15.2))
+# import math
+# print(math.ceil(15.2))
 
-from random import randint
-print(randint(1,6))
+# from random import randint
+# print(randint(1,6))
 
-def f(x):
-    print(x*5)
+# def f(x):
+#     print(x*5)
 
-f(5)
+# f(5)
 
 
-GOD = "神"
-print(GOD)
+# GOD = "神"
+# print(GOD)
 
-ooo = 30
-if 10 < 000:
-    print("s")
-else:
-    print("d")
+# ooo = 30
+# if 10 < 000:
+#     print("s")
+# else:
+#     print("d")
 
-#演習問題
-age = 20
-if age >= 20:
-    print("聖人です")
-else:
-    print("子供です")
+# #演習問題
+# age = 20
+# if age >= 20:
+#     print("聖人です")
+# else:
+#     print("子供です")
+
+# li = [10,20,30]
+# print(li)
+
+# li2 = [10,"aaa",30]
+# li2.append("php")
+# print(li2)
+
+# ppp = {"name":"すぐる","age":25}
+# print(ppp["age"])
+# ppp["aaa"] = "bbb"
+# print(ppp)
+
+# #演習問題
+# years_list =[2017,2018,2019,2020,2021]
+# print(years_list[3])
+# print(years_list[4])
+# aaa = [1111,111,111]
+# aaa.append(2222)
+# print(aaa)
+# ppp = {"name":"すぐる","age":25}
+
+# for i in ["aaa","bbb","ccc"]:
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+# for index,name in enumerate(["aaa","bbb","ccc"]):
+#     print(index,name)
+
+# ppp = {"name":"すぐる","age":25}
+# for key,value in ppp.items():
+#     print("key:{}value:{}".format(key,value))
+
+# for s in [3,2,1,"GO"]:
+#     print(s)
+
+
+# eee = []
+# for i in range(1,101):
+#     if i % 2 == 0:
+#         eee.append(i)
+
+# print(eee)
