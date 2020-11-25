21cd35667723cfcc0836bd36e66af419315782fb
Oohira Suguru <suguru.13579515@gmail.com>
2020-11-25 21:24:22 +0900

    Study python

--- chapter4.py
+++ chapter4.py
@@ -7,8 +7,8 @@ print(round(3.65))
 a = "aaa"
 print(len(a))
 
-value = input("何か入力してください")
-print(value)
+# value = input("何か入力してください")
+# print(value)
 
 import math
 print(math.ceil(15.2))
@@ -20,3 +20,20 @@ def f(x):
     print(x*5)
 
 f(5)
+
+
+GOD = "神"
+print(GOD)
+
+ooo = 30
+if 10 < 000:
+    print("s")
+else:
+    print("d")
+
+#演習問題
+age = 20
+if age >= 20:
+    print("聖人です")
+else:
+    print("子供です")
