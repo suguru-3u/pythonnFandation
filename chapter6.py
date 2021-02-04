4fbfdbb7c4cb95a9cb79d081154cb64d238fa6b7
Oohira Suguru <suguru.13579515@gmail.com>
2021-02-05 07:06:46 +0900

    Study python

--- chapter6.py
+++ chapter6.py
@@ -1,2 +1,28 @@
-nums = [1] *10
-print(nums)
+#nums = [1] *10
+#print(nums)
+
+"""
+r101 = "佐藤"
+r102 = "田中"
+r103 = "鈴木"
+r201 = "青木"
+r202 = "広田"
+r203 = "野村"
+floor1 = [r101,r102,r103]
+floor2 = [r201,r202,r203]
+apartment = [floor1,floor2]
+apartment[0][2] = "マイケル"
+print(apartment)
+print(len(apartment))
+"""
+
+pos = int(input("取り出す位置"))
+colors = ["red","bule","green","orenge"]
+
+try:
+    item = colors[pos]
+    print(item)
+except IndexError :
+    print("インデックスエラー")
+except Exception as error:
+    print(error)
