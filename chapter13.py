f8193cb252390440a218c40347cd828a45211d32
Oohira Suguru <suguru.13579515@gmail.com>
2021-02-24 09:47:40 +0900

    Study python

--- chapter13.py
+++ chapter13.py
@@ -9,14 +9,42 @@
 #     newtext = text.split(" ")
 #     print(newtext)
 
-import sys
+# import sys
 # arglist = sys.argv
 # print(arglist)
-if len(sys.argv)<2:
-    print("読み込みができませんでした")
-    sys.exit()
+# if len(sys.argv)<2:
+#     print("読み込みができませんでした")
+#     sys.exit()
 
-file = sys.argv[1]
-with open(file) as fileobj:
-    data = fileobj.read()
-    print(data)
+# file = sys.argv[1]
+# with open(file) as fileobj:
+#     data = fileobj.read()
+#     print(data)
+
+# import tkinter as tk
+# import tkinter.filedialog as fd
+
+# root = tk.Tk()
+# root.withdraw()
+
+# file = fd.askopenfilename(
+#     title = "ファイルを選んでください",
+#     filetype = [("TEXT",".txt"),("TEXT",".py"),("HTML",".html")]
+# )
+
+# if file:
+#     with open(file) as fileobj:
+#         text = fileobj.read()
+#         print(text)
+
+import sys
+import tkinter
+
+root = tkinter.Tk()
+root.title(u"Software Title")
+root.geometry("400x300")
+
+Static1 = tkinter.Label(text=u'test')
+Static1.pack()
+
+root.mainloop()
