19391c37f58bf5a19a3af990d1fc0c1e8ca25e24
Oohira Suguru <suguru.13579515@gmail.com>
2021-02-24 09:20:15 +0900

    STudy python

--- chapter13.py
+++ chapter13.py
@@ -1,5 +1,22 @@
-file = "test.txt"
-fileobj = open(file)
-text = fileobj.read()
-fileobj.close()
-print(text)
+# file = "test.txt"
+# fileobj = open(file)
+# text = fileobj.read()
+# fileobj.close()
+# print(text)
+
+# with open(file) as fileobj:
+#     text = fileobj.read()
+#     newtext = text.split(" ")
+#     print(newtext)
+
+import sys
+# arglist = sys.argv
+# print(arglist)
+if len(sys.argv)<2:
+    print("読み込みができませんでした")
+    sys.exit()
+
+file = sys.argv[1]
+with open(file) as fileobj:
+    data = fileobj.read()
+    print(data)
