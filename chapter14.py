6a07345d33814f9d3ac400c857fbee4b77180379
Oohira Suguru <suguru.13579515@gmail.com>
2021-02-26 08:50:48 +0900

    study python

--- chapter14.py
+++ chapter14.py
@@ -15,7 +15,18 @@ import math
 # plt.grid(True)
 # plt.show()
 
-x = range(0,360)
-y = [math.sin(math.radians(d)) for d in x]
-plt.plot(x,y)
+# x = range(0,360)
+# y = [math.sin(math.radians(d)) for d in x]
+# c = [math.cos(math.radians(d)) for d in x]
+# plt.plot(x,y)
+# plt.plot(x,c)
+# plt.show()
+
+x = [100,200,300,400,500]
+Y1 = [40,45,60,80,100]
+Y2 = [34,55,77,88,99]
+Y3 = [25,65,77,44,77]
+plt.plot(x,Y1,marker = "o",linestyle="-",label = "Y1")
+plt.plot(x,Y2,marker = "v",linestyle="--",label = "Y2")
+plt.plot(x,Y3,marker = "d",linestyle="-.",label = "Y3")
 plt.show()
