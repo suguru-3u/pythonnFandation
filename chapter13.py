23a4a152dd66a372945d13e5afa89bc7dcd332a7
Oohira Suguru <suguru.13579515@gmail.com>
2021-02-25 09:43:27 +0900

    Study Python

--- chapter13.py
+++ chapter13.py
@@ -80,3 +80,50 @@
         # except StopIteration:
         #     print("見つかりませんでした")
         #     break
+
+# file = "sample.txt"
+# fileobj = open(file,"w",encoding="utf_8")
+# fileobj.write("こんにちわ\n")
+# fileobj.close()
+
+# import sys
+# from datetime import datetime
+# file = "log.txt"
+
+# if len(sys.argv) < 2:
+#     sys.exit()
+
+# now = str(datetime.now())
+# memo = sys.argv[1]
+# line = "-" * 10
+
+# with open(file,"a") as fileobj:
+#     fileobj.write(now + "\n")
+#     fileobj.write(memo + "\n")
+#     fileobj.write(line + "\n")
+
+# if os.path.exists("log.txt"):
+#     print("あたり")
+
+import os
+from random import randint
+
+folder = "log.txt"
+
+def filewrite():
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    with open(folder,"a",encoding="utf_8") as fileobj:
+        num = randint(0,100)
+        fileobj.write(f"{num}が出ました\n")
+        print("ファイルを保存しました")
+
+if os.path.exists(folder):
+    while True:
+        answer = input("上書きしてもいいですか?(y / n)")
+        if answer == "y":
+            filewrite()
+        elif answer == "n":
+            break
+else:
+    filewrite()
