d4d21435905f3b0266bdc95319a92fcb65d0d577
Oohira Suguru <suguru.13579515@gmail.com>
2021-02-28 07:13:46 +0900

    Study python

--- chapter15.py
+++ chapter15.py
@@ -7,5 +7,23 @@ import numpy as np
 # b = np.array(data,dtype=str)
 # print(b.dtype)
 
-c = np.array([[1,2,3],[4,5,6]])
-print(c)
+# 
+
+# data = [1,2,3]
+# a = np.array(data).reshape(3,1)
+# print(a)
+# o = np.append(a,[[5]],axis = 0)
+# o = np.insert(a,1,99)
+# print(o)
+
+# a.ravel()
+# print(a.shape)
+# print(len(a))
+# print(a.size)
+
+# c = np.append(a,[5,7,8])
+# print(c)
+
+data = [10,20,30,40,50,60]
+a = np.array(data).reshape(2,3)
+print(a[0,1])
