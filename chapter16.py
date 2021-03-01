93932c5eced8159baa2107171cf77407b2066644
Oohira Suguru <suguru.13579515@gmail.com>
2021-03-01 14:20:08 +0900

    Study python

---
+++ chapter16.py
@@ -0,0 +1,12 @@
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+digits = datasets.load_digits()
+print(dir(digits))
+# print(digits.data.shape)
+# print(digits.data.shape)
+# print(digits.data)
+# print(digits.target)
+print(digits.data[0])
