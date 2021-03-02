527fa1dc67cabe3267562d281a133224ce89e4cd
Oohira Suguru <suguru.13579515@gmail.com>
2021-03-02 23:56:07 +0900

    Study pythin

---
+++ chapter17.py
@@ -0,0 +1,34 @@
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from sklearn import svm
+from sklearn import metrics
+
+# iris = datasets.load_iris()
+# print(dir(iris))
+# print(iris.DESCR)
+
+# x = iris.data
+# y = iris.target
+
+# print(x)
+# print(y)
+
+# print(iris.feature_names)
+
+# plt.scatter(x[:50,0],x[:50,1],color='r' ,marker='o',label='setosa')
+# plt.scatter(x[50:100,0],x[50:100,1],color='g' ,marker='+',label='setosa')
+# plt.scatter(x[100:,0],x[100:,1],color='b' ,marker='x',label='setosa')
+# plt.title("Yes")
+# plt.xlabel('sepal length(cm)')
+# plt.ylabel('sepal width(cm)')
+# plt.legend()
+# plt.show()
+
+
+ plt.title("Yes")
+plt.xlabel('sepal length(cm)')
+plt.ylabel('sepal width(cm)')
+plt.legend()
+plt.show()
