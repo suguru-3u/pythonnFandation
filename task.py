b7e4999c2d3dfa2474f4d14a733e249c09bb2ed8
Oohira Suguru <suguru.13579515@gmail.com>
2021-03-03 09:24:35 +0900

    Study python tikner

--- task.py
+++ task.py
@@ -1,13 +1,20 @@
 import sys
-import Tkinter
+import tkinter as tk
 
 
-root = Tkinter.Tk()
-root.title(u"Software Title")
+root = tk.Tk()
+root.title(u"タスク管理ファイル")
 root.geometry("400x300")
 
-#ラベル
-Static1 = Tkinter.Label(text=u'test')
+#画面上部
+Static1 = tk.Label(text=u'本日のタスクを記入してください')
 Static1.pack()
+
+#入力欄
+EditBox = tk.Entry(width=50)
+EditBox.pack()
+
+#　入力された文字を取得
+value = EditBox.get()
 
 root.mainloop()
