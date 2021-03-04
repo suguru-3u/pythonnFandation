fdb8537a7a413339583d0b3c65991e76acbb5bf4
suguru-3u <suguru.13579515@gmail.com>
2021-03-04 16:47:01 +0900

    Study python

--- task.py
+++ task.py
@@ -13,6 +13,10 @@ class App:
         self.input_area = InputArea(self.master)
         self.input_area.pack()
 
+        # 表示エリア
+        self.list_area = ListArea(self.master)
+        self.list_area.pack()
+
     def mainloop(self):
         self.master.mainloop()
 
@@ -40,6 +44,32 @@ class InputArea(tk.Frame):
 
     def _click_add_btn(self):
         print('add',self.entry.get())
+
+class ListArea(tk.Frame):
+    """
+    TODOリストの表示エリア
+    ユーザーが入力したTODOが表示される
+    """
+
+    def __init__(self,master):
+        super(ListArea,self).__init__(master)
+
+        # リストアの作成
+        self.listbox = tk.Listbox(self,height=5)
+        self.listbox.pack()
+
+        # 削除のボタンの作成
+        self.del_btn = tk.Button(self,text='削除',command=self._click_del_btn)
+        self.del_btn.pack()
+    
+    def _click_del_btn(self):
+        """
+        選択中のTODOを削除する
+        """
+        print('delete selection')
+        sel = self.listbox.curselection()
+        for i in sel[::-1]:
+            self.listbox.delete(i)
 
 
 def main():
