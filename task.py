27511bf062357e39447311be414c7ab8563c82f4
suguru-3u <suguru.13579515@gmail.com>
2021-03-04 17:00:10 +0900

    Study python

--- task.py
+++ task.py
@@ -2,9 +2,13 @@ import sys
 import tkinter as tk
 
 
+# -*- coding: utf-8 -*-
+import tkinter as tk
+
+
 class App:
     def __init__(self):
-        #初期化処理
+        # ウィンドウを初期化
         self.master = tk.Tk()
         self.master.title('TODOアプリ')
         self.master.geometry('400x300')
@@ -19,14 +23,12 @@ class App:
         self.list_area = ListArea(self.master)
         self.list_area.pack(side='bottom', expand=True, fill='both')
 
-
     def mainloop(self):
+        # masterに処理を委譲
         self.master.mainloop()
 
     def click_add_btn(self):
         """
-        このメソッドを追加
-
         入力エリアの追加ボタンが押されたら呼ばれる
         入力エリアの入力値をリストエリアに追加する
         """
@@ -42,27 +44,28 @@ class InputArea(tk.Frame):
     ユーザーが入力したTODOテキストを追加ボタンでリストに追加するのが役割
     """
 
-    def __init__(self,master):
-        super(InputArea,self).__init__(master)
+    def __init__(self, master):
+        super(InputArea, self).__init__(master)
 
-         # ハンドル
-        self.click_add_btn = None 
+        # ハンドル
+        self.click_add_btn = None
 
-        # ラベル
-        self.label = tk.Label(self,text="TODO")
+        # ラベルの作成
+        self.label = tk.Label(self, text='TODO')
         self.label.pack(side='left')
 
         # 入力行の作成
         self.entry = tk.Entry(self)
-        self.entry.pack(side='left')
+        self.entry.pack(side='left', expand=True, fill='x')
 
-        # 追加ボタン
-        self.add_btn = tk.Button(self,text='追加',command=self._click_add_btn)
+        # 追加ボタンの作成
+        self.add_btn = tk.Button(self, text='追加', command=self._click_add_btn)
         self.add_btn.pack(side='left')
 
     def _click_add_btn(self):
-         if self.click_add_btn:
+        if self.click_add_btn:
             self.click_add_btn()
+
 
 class ListArea(tk.Frame):
     """
@@ -70,17 +73,17 @@ class ListArea(tk.Frame):
     ユーザーが入力したTODOが表示される
     """
 
-    def __init__(self,master):
-        super(ListArea,self).__init__(master)
+    def __init__(self, master):
+        super(ListArea, self).__init__(master)
 
-        # リストアの作成
-        self.listbox = tk.Listbox(self,height=5)
-        self.listbox.pack()
+        # リストの作成
+        self.listbox = tk.Listbox(self, height=5)
+        self.listbox.pack(side='top', expand=True, fill='both')
 
-        # 削除のボタンの作成
-        self.del_btn = tk.Button(self,text='削除',command=self._click_del_btn)
-        self.del_btn.pack()
-    
+        # 削除ボタンの作成
+        self.del_btn = tk.Button(self, text='削除', command=self._click_del_btn)
+        self.del_btn.pack(side='bottom', fill='x')
+
     def _click_del_btn(self):
         """
         選択中のTODOを削除する
@@ -90,12 +93,10 @@ class ListArea(tk.Frame):
             self.listbox.delete(i)
 
 
-
-
 def main():
     app = App()
     app.mainloop()
 
+
 if __name__ == '__main__':
     main()
-
