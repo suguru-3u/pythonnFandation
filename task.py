177c24fc4ff53c5ceaaba873160a821e7103749c
suguru-3u <suguru.13579515@gmail.com>
2021-03-04 16:24:34 +0900

    Study python

--- task.py
+++ task.py
@@ -2,20 +2,38 @@ import sys
 import tkinter as tk
 
 
-root = tk.Tk()
-root.title(u"タスク管理ファイル")
-root.geometry("400x300")
+class App:
+    def __init__(self):
+        #初期化処理
+        self.master = tk.Tk()
+        self.master.title('TODOアプリ')
+        self.master.geometry('400x300')
 
-#画面上部
-Static1 = tk.Label(text=u'本日のタスクを記入してください')
-Static1.pack()
+    def mainloop(self):
+        self.master.mainloop()
 
-#入力欄
-EditBox = tk.Entry(width=50)
-EditBox.pack()
+def main():
+    app = App()
+    app.mainloop()
 
-#　入力された文字を取得
-value = EditBox.get()
-print(value)
+if __name__ == '__main__':
+    main()
 
-root.mainloop()
+
+# root = tk.Tk()
+# root.title(u"タスク管理ファイル")
+# root.geometry("400x300")
+
+# #画面上部
+# Static1 = tk.Label(text=u'本日のタスクを記入してください')
+# Static1.pack()
+
+# #入力欄
+# EditBox = tk.Entry(width=50)
+# EditBox.pack()
+
+# #　入力された文字を取得
+# value = EditBox.get()
+# print(value)
+
+# root.mainloop()
