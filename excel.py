027f5052adea8417bdf872fb31790a283c4bd27a
suguru-3u <suguru.13579515@gmail.com>
2021-03-05 14:11:49 +0900

    Study python

--- excel.py
+++ excel.py
@@ -14,3 +14,15 @@ for i in range(3,9):
         suppliers.append(cell_value)
 
 print(suppliers)
+
+for i in suppliers:
+    transaction_amount = 0
+    for j in range(3,9):
+        suppliers = sheet.cell(row=j,column=2).value
+
+        if suppliers == i :
+            transaction_amount += sheet.cell(row=j,column=6).value
+
+    transaction_amounts.append(transaction_amount)
+
+print(transaction_amounts)
