a3e7597df42c82e8a776574ff8fa0225b7c667b6
Oohira Suguru <suguru.13579515@gmail.com>
2021-03-07 14:59:50 +0900

    Study python

--- scraping.py
+++ scraping.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-site = requests.get(‘https://www.google.com’)
-print(site.text)
+site = requests.get('https://www.google.com')
+
+data = BeautifulSoup(site.text, 'html.parser')
+
+print(data.title)
+print(data.title.text)
